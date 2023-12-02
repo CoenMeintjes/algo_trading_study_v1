c6485093c0c8d5ad2663aeef8073019d6491737f
Coen <chmeintjes@gmail.com>
2023-12-02 21:48:08 +0200

    update queries

--- db/queries.sql
+++ db/queries.sql
@@ -1,3 +1,5 @@
+-- local connection psql -h algo-01.postgres.database.azure.com -p 5432 -U postgres 
+
 SELECT  order_id, symbol, pair, pair_order, status, spread, orig_qty, side, position_side, update_time
 FROM orders
 ORDER BY update_time ASC;
