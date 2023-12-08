3ad59734d22bda9bc5f5d421f762984c7f2cbce4
Coen <chmeintjes@gmail.com>
2023-12-08 16:00:37 +0200

    attempt 2 diff sql client

--- function_app.py
+++ function_app.py
@@ -10,7 +10,7 @@ app = func.FunctionApp()
 @app.timer_trigger(
     schedule="0 1 0 * * *", 
     arg_name="myTimer",
-    run_on_startup=True, ####### CHANGE TO FALSE FOR PRODUCTION
+    run_on_startup=False, ####### CHANGE TO FALSE FOR PRODUCTION
     use_monitor= False 
 ) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
