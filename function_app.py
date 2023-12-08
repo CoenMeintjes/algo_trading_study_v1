661be9a7aee02ce75dcdcb9978a8f67519ef5004
Coen <chmeintjes@gmail.com>
2023-12-08 06:01:26 +0200

    test with moved dependencies into def

--- function_app.py
+++ function_app.py
@@ -1,12 +1,5 @@
-from loguru import logger
 import azure.functions as func
-from datetime import datetime, timedelta
-from data_fetch import data_fetch
-from execution import execution_model
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from azure_utils import get_secret
-import os
+import logging
 
 # Instantiate the app
 app = func.FunctionApp()
@@ -15,12 +8,22 @@ app = func.FunctionApp()
 # {second} {minute} {hour} {day} {month} {day-of-week}
 # function will trigger at 1 min past midnight utc every day = '0 1 0 * * *'
 @app.timer_trigger(
-    schedule='0 1 0 * * *', 
-    arg_name="mytimer",
-    run_on_startup=False,
-    use_monitor=False
+    schedule="0 1 0 * * *", 
+    arg_name="myTimer",
+    run_on_startup=True, ####### CHANGE TO FALSE FOR PRODUCTION
+    use_monitor= False 
 ) 
-def algo_v1(mytimer: func.TimerRequest) -> None:
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    from datetime import datetime, timedelta
+    from data_fetch import data_fetch
+    from execution import execution_model
+    from sendgrid import SendGridAPIClient
+    from sendgrid.helpers.mail import Mail
+    from utils import get_secret
+    import os
+
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
 
     ### ENVIRONMENT VARIABLES
     try:
@@ -30,38 +33,38 @@ def algo_v1(mytimer: func.TimerRequest) -> None:
         binance_secret = get_secret('binance-secret')  
 
     except Exception as e:
-        logger.error(f'Error fetching keys: {e}')
+        logging.error(f'Error fetching keys: {e}')
 
     today = (datetime.now()).date()
     yesterday = today - timedelta(days= 1)
 
     ### RUN DATA FETCH    
     try: 
-        logger.info(f'Fetching data up to end of {yesterday}')
+        logging.info(f'Fetching data up to end of {yesterday}')
         data_fetch(
             start= yesterday,
             end= yesterday,
             connection_string= connection_string
         )
-        logger.info(f'Data fetch successful.')
+        logging.info(f'Data fetch successful.')
 
     except Exception as e:
-        logger.error(f'Error running the data_fetch | {e}')
+        logging.error(f'Error running the data_fetch | {e}')
 
     ### RUN THE EXECUTION MODEL
     try:
-        logger.info(f'Running execution_model script...')
+        logging.info(f'Running execution_model script...')
         execution_model(
             binance_api = binance_api,
             binance_secret= binance_secret,
             connection_string= connection_string,
         )
-        logger.info(f'Execution model successful.')
+        logging.info(f'Execution model successful.')
     except Exception as e:
-        logger.error(f'Error running the execution model | {e}')
+        logging.error(f'Error running the execution model | {e}')
 
     ### SEND EMAIL NOTIFICATION 
-    logger.info(f'Sending notification')
+    logging.info(f'Sending notification')
     message = Mail(
         from_email='chmeintjes@gmail.com',
         to_emails='chmeintjes@gmail.com',
@@ -71,10 +74,10 @@ def algo_v1(mytimer: func.TimerRequest) -> None:
     try:
         sg = SendGridAPIClient(sg_api)
         response = sg.send(message)
-        logger.info(f'sendgrid response status code | {response.status_code}')
+        logging.info(f'sendgrid response status code | {response.status_code}')
 
     except Exception as e:
-        logger.error(e)
+        logging.error(e)
 
-    logger.info(f'Function run complete.')
+    logging.info(f'Function run complete.')
 
